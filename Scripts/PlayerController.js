7aae17553d02f991c2e0c1b89b6085f63abb95e4
jennypavlova <jennypavlova94@gmail.com>
2017-02-17 01:10:04 +0200

    Moving the Player

---
+++ Scripts/PlayerController.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+
+public class PlayerController extends MonoBehaviour {
+    var rb: Rigidbody;
+    var speed : float = 30.0;
+    function Start() {
+        rb = GetComponent.<Rigidbody>();
+    }
+
+    function FixedUpdate () {
+        var horizontal : float= Input.GetAxis ("Horizontal");
+        var vertical : float= Input.GetAxis ("Vertical");
+        Debug.Log(horizontal);
+        Debug.Log(vertical);
+        rb.AddForce(Vector3(horizontal, 0.0, vertical)*speed);
+    }
+}
