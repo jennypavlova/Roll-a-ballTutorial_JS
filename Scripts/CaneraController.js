f5c6caa4d4d069646da81465446819dea397cf00
jennypavlova <jennypavlova94@gmail.com>
2017-02-17 01:19:10 +0200

    Moving the Camera

---
+++ Scripts/CaneraController.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var player : GameObject;
+var offset : Vector3;
+
+function Start () {
+    offset = transform.position - player.transform.position;
+}
+
+function LateUpdate () {
+    print(player.transform.position.x);
+    transform.position = player.transform.position + offset;
+}
